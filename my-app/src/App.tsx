8c640de0638b10cc9beabe295b83f6bd017e841b
Segway-tech <83791518+Segway-tech@users.noreply.github.com>
2025-05-02 16:44:19 +0200

    Added home

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <title>Test</title>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -26,7 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Ciggpaus
       </p>
     </>
   )
